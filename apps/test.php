af68538f70048c4a8b0f404babf21f8088b189eb
ludovicdannhoff <ludovic.dannhoff@gmail.com>
2016-03-03 11:01:40 +0100

    ajout ticket

---
+++ apps/test.php
@@ -0,0 +1,15 @@
+<?php
+$query = "SELECT * FROM tickets WHERE id_etat_tickets = '3'";// On déclare une variable qui contiendra notre requête SQL
+$result = mysqli_query($db, $query);// On exécute notre requête SQL
+$number = 1;
+while ($tickets = mysqli_fetch_assoc($result))// On récupère les résultats de notre requête un par un
+{
+	$id = $tickets['id_tickets'];
+	$date = $tickets['date_creation_tickets'];
+	$title = $tickets['titre_tickets'];
+	$content = $tickets['content_tickets'];
+	$deadline = $tickets['deadline_tickets'];
+	$number++;
+	require('views/ticket.phtml');
+}
+?>
